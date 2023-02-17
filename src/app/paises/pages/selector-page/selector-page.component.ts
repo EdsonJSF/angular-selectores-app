402ba721ce35e68d77417baf7d49d5e9ca2c40cf
Edsonjsf <edsonjsf1990@gmail.com>
2023-02-17 13:21:27 -0400

    Formulario reactivo - primer selector

--- src/app/paises/pages/selector-page/selector-page.component.ts
+++ src/app/paises/pages/selector-page/selector-page.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, Validators, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-selector-page',
   templateUrl: './selector-page.component.html',
-  styleUrls: ['./selector-page.component.scss']
+  styleUrls: ['./selector-page.component.scss'],
 })
 export class SelectorPageComponent implements OnInit {
+  miFormulario: FormGroup = this.fb.group({
+    region: ['', Validators.required],
+  });
 
-  constructor() { }
+  constructor(private fb: FormBuilder) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  guardar() {
+    console.log(this.miFormulario.value);
   }
-
 }
--- src/app/paises/paises.module.ts
+++ src/app/paises/paises.module.ts
@@ -1,19 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { PaisesRoutingModule } from './paises-routing.module';
 import { SelectorPageComponent } from './pages/selector-page/selector-page.component';
 import { PaisesComponent } from './services/paises/paises.component';
 
-
 @NgModule({
-  declarations: [
-    SelectorPageComponent,
-    PaisesComponent
-  ],
-  imports: [
-    CommonModule,
-    PaisesRoutingModule
-  ]
+  declarations: [SelectorPageComponent, PaisesComponent],
+  imports: [CommonModule, ReactiveFormsModule, PaisesRoutingModule],
 })
-export class PaisesModule { }
+export class PaisesModule {}
